d49c7c666fd075ae82eb4e237f8900513c479bdf
Simon <semen.zelenskiy@gmail.com>
2019-11-04 09:50:38 +0200

    Updates

---
+++ precache-manifest.67a6ddb0c530a01d0277ba11352a4828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165a7c05d9ca9b27973d5da7766bf42b",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "91a09a90e60489712924",
+    "url": "/react_add-todo-form/static/css/main.82b06346.chunk.css"
+  },
+  {
+    "revision": "f8f02e1f768454e3da7a",
+    "url": "/react_add-todo-form/static/js/2.4b7a3035.chunk.js"
+  },
+  {
+    "revision": "91a09a90e60489712924",
+    "url": "/react_add-todo-form/static/js/main.2d4faa56.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
